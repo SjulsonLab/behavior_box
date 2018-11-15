81afab63c041342b33187c8a7a632eabbf58dc1f
Luke Sjulson <lukesjulson@gmail.com>
2018-11-15 12:47:39 -0500

    changed the rest of code to be v6 instead of v5

--- DUE_greenStandard_v6/DUE_greenStandard_v6.ino
+++ DUE_greenStandard_v6/DUE_greenStandard_v6.ino
@@ -44,7 +44,7 @@
 #include "libraries/Timer-master/Timer.cpp" // https://playground.arduino.cc/Code/Timer
 //#include <string>
 #include <avr/pgmspace.h> // might be required to store waveforms on flash instead of RAM
-#include "toInclude/greenStandardFunctions_v5.cpp"
+#include "toInclude/greenStandardFunctions_v6.cpp"
 
 // state definitions
 #define standby       		1  // standby - the inactive state
