0a0d0334c23a4762cbd35fe0bcdbddfe5c05faf7
Luke Sjulson <lukesjulson@gmail.com>
2018-12-20 17:55:19 -0500

    modified getEventTimes() so that it doesn't rely on any bash shell commands

--- matlab/include/getEventTimes.m
+++ matlab/include/getEventTimes.m
@@ -3,18 +3,31 @@ function T = getEventTimes(textString, fname)
 % function T = getEventTimes(textString, fname)
 %
 % This function searches for textString in the logfile called fname.
-% if fname is not provided, it searches all .txt files in the current 
-% directory.
 % It then returns the first column (which is the timestamp for
 % behavior box log files). 
 %
 % Luke Sjulson, 2018-12-20
 
-if nargin<2
-	fname = '*.txt';
-end
 
-winbash(['grep -i ' textString ' ' fname ' | cut -f 1 -d '';'' > ' textString '.tmp'], 'silent');
-T = load([textString '.tmp']) ./ 1000; % convert from ms to seconds
-winbash(['rm ' textString '.tmp']);
+% %% testing
+% fname = 'D1R96Male246_181203_154542.txt';
+% textString = 'leftReward';
+
+
+fid = fopen(fname, 'rt');
+idx = 1;
+while ~feof(fid)
+	filetemp = fgetl(fid);
+	if contains(filetemp, textString, 'IgnoreCase', true)
+		T(idx) = str2num(filetemp(1:find(filetemp==';', 1)));
+		idx = idx + 1;
+	end
+end
+fclose(fid);
+
+
+% % old code, works on some windows machines but not others
+% winbash(['grep -i ' textString ' ' fname ' | cut -f 1 -d '';'' > ' textString '.tmp'], 'silent');
+% T = load([textString '.tmp']) ./ 1000; % convert from ms to seconds
+% winbash(['rm ' textString '.tmp']);
 
