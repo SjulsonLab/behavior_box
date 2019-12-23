17869c1153ae8605072fc19d59a630aff8aed2c5
Luke Sjulson <lukesjulson@gmail.com>
2019-12-23 16:45:32 -0500

    turned off debugging

--- DUE_greenStandard_v7/toInclude/greenStandardFunctions_v7.cpp
+++ DUE_greenStandard_v7/toInclude/greenStandardFunctions_v7.cpp
@@ -9,7 +9,7 @@ TO DO:
 */
 
 // for debugging
-#define DEBUG   //If you comment out this line, the DPRINT & DPRINTLN lines are defined as blank.
+//#define DEBUG   //If you comment out this line, the DPRINT & DPRINTLN lines are defined as blank.
 #ifdef DEBUG    //Macros are usually in all capital letters.
 #define DPRINT(...)    Serial.print(__VA_ARGS__)     //DPRINT is a macro, debug print
 #define DPRINTLN(...)  Serial.println(__VA_ARGS__)   //DPRINTLN is a macro, debug print with new line
